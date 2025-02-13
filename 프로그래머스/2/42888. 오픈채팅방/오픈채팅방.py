7d4455fa6049f98a1c96cd45eb2dfb0dc53e3385
dpwls <130109502+dpwls02142@users.noreply.github.com>
2025-02-13 13:56:00 +0900

    [level 2] Title: 오픈채팅방, Time: 140.08 ms, Memory: 57 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,21 @@
+def solution(record):
+    user_dict = {}
+    log = []
+    
+    for entry in record:
+        data = entry.split()
+        command, uid = data[0], data[1]
+
+        if command == "Enter":
+            nickname = data[2]
+            user_dict[uid] = nickname
+            log.append((uid, "님이 들어왔습니다."))
+
+        elif command == "Leave":
+            log.append((uid, "님이 나갔습니다."))
+
+        elif command == "Change":
+            nickname = data[2]
+            user_dict[uid] = nickname
+            
+    return [user_dict[uid] + action for uid, action in log]
