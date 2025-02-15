7106a1b4cdbb041a4c61b73a2c06189eebf2bb16
dpwls <130109502+dpwls02142@users.noreply.github.com>
2025-02-15 13:37:48 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,5 +1,9 @@
 def solution(spell, dic):
-    for i in dic:
-        if set(spell) == set(i):
-            return 1
-    return 2
+    for word in dic:  # dic의 단어 하나씩 가져오기
+        cnt = 0  # spell의 문자가 몇 개 일치하는지 세는 변수
+        for s in spell:  # spell의 문자 하나씩 확인
+            if s in word:  # word에 해당 문자가 포함되어 있으면
+                cnt += 1  
+        if cnt == len(spell):  # spell의 모든 문자가 다 포함되어 있으면
+            return 1  
+    return 2  # 끝까지 확인했는데 없으면 2 반환
