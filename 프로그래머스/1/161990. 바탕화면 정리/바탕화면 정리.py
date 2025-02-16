baa7aef222a462caa3afe32d8c8bb635401663c4
dpwls <130109502+dpwls02142@users.noreply.github.com>
2025-02-16 20:26:52 +0900

    [level 1] Title: 바탕화면 정리, Time: 1.87 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,26 +1,15 @@
 def solution(wallpaper):
-    
+    # 아이템이 있는 좌표의 인덱스만 a에 저장
     a = []
-    for i in wallpaper:
-        a.append(i)
+    for i in range (len(wallpaper)):
+        for j in range (len(wallpaper[i])):
+            if wallpaper[i][j] == '#':
+                a.append([i, j])
 
-    c = []
-    for i in range (len(a)):
-        for j in range (len(a[i])):
-            if a[i][j] == '#':
-                c.append([i, j])
+    x_min = min(a, key=lambda a: a[0])[0]
+    x_max = max(a, key=lambda a: a[0])[0]
+    
+    y_min = min(a, key=lambda a: a[1])[1]
+    y_max = max(a, key=lambda a: a[1])[1]
 
-    # 초기값을 첫 번째 좌표값으로
-    x_min, x_max =  c[0][0], c[0][0]
-    y_min, y_max = c[0][1], c[0][1]
-
-    for x, y in c:
-        if x < x_min:
-            x_min = x
-        if x > x_max:
-            x_max = x
-        if y < y_min:
-            y_min = y
-        if y > y_max:
-            y_max = y
     return ([x_min, y_min, x_max+1, y_max+1])
