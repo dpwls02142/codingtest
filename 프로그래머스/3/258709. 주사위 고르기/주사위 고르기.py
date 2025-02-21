8fa3b4e4392b6038a45051d54288a9d19eaf5ad4
dpwls <130109502+dpwls02142@users.noreply.github.com>
2025-02-21 20:01:45 +0900

    [level 3] Title: 주사위 고르기, Time: 1710.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -5,26 +5,34 @@ def solution(dice):
     n = len(dice)
     a = n // 2
     best_win_count = -1
-    
-    
-    # A가 선택할 수 있는 모든 주사위 조합
-    # n 범위 내에서 a만큼 뽑아서 조합을 생성한다.
+    best_combo = []
+
     for a_combo in combinations(range(n), a):
-        # b는 a_combo에 있는 값 제외 남은 값을 넣는다.
         b_combo = [i for i in range(n) if i not in a_combo]
         
         win_count = 0
         
-        a_sums = sorted([sum(rolls) for rolls in product(*[dice[i] for i in a_combo])])
-        b_sums = sorted([sum(rolls) for rolls in product(*[dice[i] for i in b_combo])])
+        a_sums = []
+        b_sums = []
+
+        a_dice = [dice[i] for i in a_combo]  
+        for a_rolls in product(*a_dice):    
+            a_sum = sum(a_rolls)             
+            a_sums.append(a_sum)             
         
-        # 이진 탐색을 활용하여 A가 이길 수 있는 경우의 수 계산
+        b_dice = [dice[i] for i in b_combo]
+        for b_rolls in product(*b_dice):     
+            b_sum = sum(b_rolls)             
+            b_sums.append(b_sum)             
+        
+        a_sums.sort()
+        b_sums.sort()
+        
         for a_sum in a_sums:
             win_count += bisect_left(b_sums, a_sum)
         
-        # a가 이기는 경우의 수 중에 그 값이 가장 큰 값
-        if win_count >= best_win_count:
+        if win_count > best_win_count:
             best_win_count = win_count
-            best_combo = [i + 1 for i in a_combo] # 인덱스가 1부터 시작하니까
-    
+            best_combo = [i + 1 for i in a_combo]
+
     return sorted(best_combo)
