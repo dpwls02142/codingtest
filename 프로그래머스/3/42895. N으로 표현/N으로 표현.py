67862a29925d99336bcfca932b68e068d5a0eb6e
dpwls <130109502+dpwls02142@users.noreply.github.com>
2025-02-25 13:00:10 +0900

    [level 3] Title: N으로 표현, Time: 10.23 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,6 +1,6 @@
 def solution(N, number):
     dp = [set() for _ in range(9)]
-
+    
     # i개의 N으로 만들 수 있는 초기값
     for i in range(1, 9):
         dp[i].add(int(str(N) * i))
@@ -11,7 +11,8 @@ def solution(N, number):
             for op1 in dp[j]:
                 for op2 in dp[i - j]:
                     dp[i].add(op1 + op2)
-                    dp[i].add(op1 - op2)
+                    if op1 - op2 >= 0:
+                        dp[i].add(op1 - op2)
                     dp[i].add(op1 * op2)
                     if op2 != 0:
                         dp[i].add(op1 // op2)
