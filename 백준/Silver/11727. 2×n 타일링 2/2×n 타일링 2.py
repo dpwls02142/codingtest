11a7361096a54195349dc977e5f2f5bc3f1068b7
dpwls <130109502+dpwls02142@users.noreply.github.com>
2025-02-26 11:54:54 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+if n == 1:
+    print(1)
+    sys.exit(0)
+dp = [0] * (n + 1)
+dp[1] = 1
+dp[2] = 3
+for i in range (3, n + 1):
+    dp[i] = dp[i-1] + 2 * dp[i-2]
+print(dp[n] % 10007)
