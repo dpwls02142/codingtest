dbd9a1da3cf94dee5cf4976160531426abc346bf
dpwls <130109502+dpwls02142@users.noreply.github.com>
2025-04-08 21:16:36 +0900

    [Silver IV] Title: 저지, Time: 2408 ms, Memory: 184988 KB -BaekjoonHub

---
+++ 백준/Silver/10774. 저지/저지.py
@@ -0,0 +1,28 @@
+import sys
+j = int(sys.stdin.readline().rstrip())
+a = int(sys.stdin.readline().rstrip())
+cnt = 0
+size = {}
+
+def size_to_num(s):
+    if s == 'S':
+        return 0
+    elif s == 'M':
+        return 1
+    else:  # 'L'
+        return 2
+
+for i in range(1, j+1):
+    value = sys.stdin.readline().rstrip()
+    size[i] = [size_to_num(value), value]
+
+for i in range(a):
+    want_size, want_num = sys.stdin.readline().rstrip().split()
+    want_num = int(want_num)
+    want_size_num = size_to_num(want_size)
+    
+    if want_num in size and want_size_num <= size[want_num][0]:
+        del size[want_num]
+        cnt += 1
+
+print(cnt)
